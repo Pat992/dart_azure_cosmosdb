ea4a1f718e8c547f16de49be1f5887abb3819dea
Patrick Hettich <patrick_hettich@hotmail.com>
2023-02-10 16:53:17 +0100

    Initial commit

---
+++ example/dart_azure_cosmosdb_example.dart
@@ -0,0 +1,6 @@
+import 'package:dart_azure_cosmosdb/dart_azure_cosmosdb.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/dart_azure_cosmosdb.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library dart_azure_cosmosdb;
+
+export 'src/dart_azure_cosmosdb_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
