6030f06deed6f46214a58f8ce6984692e01abe33
Patrick Hettich <patrick_hettich@hotmail.com>
2023-03-01 18:31:50 +0100

    created exports

--- lib/dart_azure_cosmosdb.dart
+++ lib/dart_azure_cosmosdb.dart
@@ -3,6 +3,7 @@
 /// More dartdocs go here.
 library dart_azure_cosmosdb;
 
-export 'src/dart_azure_cosmosdb_base.dart';
+export 'package:dart_azure_cosmosdb/src/dart_azure_cosmosdb_base.dart';
+export 'package:dart_azure_cosmosdb/src/infrastructure/models/models_barrel.dart';
 
 // TODO: Export any libraries intended for clients of this package.
